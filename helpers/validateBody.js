62120d4fea8ccf396b70b1a16583a713298486d6
Exx1piee <mishapron2008@gmail.com>
2024-04-02 18:46:51 +0300

    a

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -1,15 +1,14 @@
 import HttpError from "./HttpError.js";
-
 const validateBody = (schema) => {
-  const func = (req, _, next) => {
+  const validateReq = (req, _, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
+    } else {
+      next();
     }
-    next();
   };
 
-  return func;
+  return validateReq;
 };
-
 export default validateBody;
