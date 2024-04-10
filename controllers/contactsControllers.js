5c72900bee89c09ee734bfbba8fc7ba0ee7f7cd1
Exx1piee <mishapron2008@gmail.com>
2024-04-10 20:09:11 +0300

    refractoring code , adding mangolib

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,63 +1,50 @@
 import { ctrlWrapper } from "../helpers/ctrlWrapper.js";
-import * as contactsService from "../services/contactsServices.js";
+import contactsServices from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
 
-const getAllContacts = async (req, res) => {
-  const result = await contactsService.listContacts();
-
+const getAll = async (req, res) => {
+  const result = await contactsServices.listContacts();
   res.json(result);
 };
 
-const getOneContact = async (req, res) => {
+const getById = async (req, res) => {
   const { id } = req.params;
-  const result = await contactsService.getContactById(id);
-
-  if (!result) {
-    throw HttpError(404);
-  }
-
+  const result = await contactsServices.getContactById(id);
+  if (!result) throw HttpError(404, `Not found`);
   res.json(result);
 };
 
-const createContact = async (req, res) => {
-  const result = await contactsService.addContact(req.body);
-
+const addContact = async (req, res) => {
+  const result = await contactsServices.addContact(req.body);
   res.status(201).json(result);
 };
 
 const updateContact = async (req, res) => {
   const { id } = req.params;
-
-  if (!Object.keys(req.body).length) {
-    return res
-      .status(400)
-      .json({ message: "Body must have at least one field" });
-  }
-
-  const result = await contactsService.updateContact(id, req.body);
-
-  if (!result) {
-    throw HttpError(404);
-  }
-
+  const result = await contactsServices.updateContactById(id, req.body);
+  if (!result) throw HttpError(404, `Not found`);
   res.json(result);
 };
 
 const deleteContact = async (req, res) => {
   const { id } = req.params;
-  const result = await contactsService.removeContact(id);
+  const result = await contactsServices.removeContactById(id);
+  if (!result) throw HttpError(404, `Not found`);
+  res.json(result);
+};
 
-  if (!result) {
-    throw HttpError(404);
-  }
-
-  res.status(204).json();
+const updateContactStatus = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsServices.updateContactStatusById(id, req.body);
+  if (!result) throw HttpError(404, `Not found`);
+  res.json(result);
 };
 
 export default {
-  getAllContacts: ctrlWrapper(getAllContacts),
-  getOneContact: ctrlWrapper(getOneContact),
-  createContact: ctrlWrapper(createContact),
-  updateContact: ctrlWrapper(updateContact),
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
   deleteContact: ctrlWrapper(deleteContact),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateContactStatus: ctrlWrapper(updateContactStatus),
 };
